9215cc38042e6f87522c16d49895306c38a87fb4
Elías De Fabrittis <defa@outlook.com.ar>
2023-01-25 18:18:01 -0300

    primer commit del proyecto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import { Footer } from "./components/Footer/Footer";
+import './App.css';
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
